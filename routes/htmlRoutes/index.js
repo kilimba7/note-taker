4d89d59091e6a53c8799afbb87e4dab802480096
Kilimba <dyaul003@umn.edu>
2022-07-17 17:28:11 -0500

    test

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const router = require('express').Router();
+
+
+// This will return the index html when a URL is entered || notes html or a wildcard 
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -1,82 +1,18 @@
-// Installing all the required modules and setting a path for this port 
-const fs = require('fs');
-const path = require('path');
-const PORT = process.env.PORT || 3000;
 const express = require('express');
-var app = express();
 
-app.use(express.static('develop'));
+const PORT = process.env.PORT || 3000;
+const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
-const { notes } = require('./develop/db/db.json');
+app.use(express.static('./public'));
 
-
-
-
-function findByTitle(title, notesArray) {
-    const result = notesArray.filter(note => note.title === title)[0];
-    return result;
-}
-
-function createNewNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, './develop/db/db.json'),
-        JSON.stringify({ notes: notesArray }, null, 2)
-    );
-    // return finished code to post route for response
-    return note;
-  }
-
-
-
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-});
-
-
-
-
-app.get('/api/notes/:title', (req, res) => {
-    const result = findByTitle(req.params.title, notes);
-    if (result) {
-    res.json(result);
-    } else {
-        res.send(404)
-;    }
-});
-
-app.post('/api/notes', (req, res) => {
-    // req.body is where our incoming content will be
-   
-    // set title based on what the next index of the array will be
-   req.body.title = notes.length.toString();
-   
-   const note = createNewNote(req.body, notes);
-   res.json(note);
-  
-});
-
-
-
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './develop/index.html')); 
-  });
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './develop/notes.html'));
-});
-
-app.get('*', (req, res) => {
-res.sendFile(path.join(__dirname, './develop/index.html'));
-});
+// Use apiRoutes
+app.use(apiRoutes);
+app.use(htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
